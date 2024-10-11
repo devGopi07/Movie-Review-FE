6e75d7e400f03b159b5b559c05d634a1922eb804
Gopinath Rajendran <gopinathofficial16@gmail.com>
2024-10-11 23:54:23 +0530

    changed be deployed url

--- src/global.js
+++ src/global.js
@@ -1,3 +1,5 @@
 // export const url = "https://638f3a564ddca317d7f213a2.mockurl.io";
 // export const url = "http://localhost:4000";
-export const url = "https://movie-review-csqv.onrender.com";
+// export const url = "https://movie-review-csqv.onrender.com";
+export const url = "https://movie-review-be.onrender.com";
+// export const url = "http://localhost:8000";
